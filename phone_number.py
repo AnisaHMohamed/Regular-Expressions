5a99b2dfe9f8e99e5734bfa62442b8fc78b2813c
Anisa <anisahamohamed@gmail.com>
2020-06-10 15:46:27 -0400

    add comments

--- phone_number.py
+++ phone_number.py
@@ -1,4 +1,6 @@
 import re
+# create regex pattern
 phoneNumRegex = re.compile(r'\d{3}-\d{3}-\d{4}')
+# search using pattern
 myNum = phoneNumRegex.search('My number is 949-459-4012.')
 print('Phone number found: ' + myNum.group())
