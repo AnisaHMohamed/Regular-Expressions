835de43d58c8c4f66824d9aac5eab2f4924fc0df
Anisa <anisahamohamed@gmail.com>
2020-06-10 15:48:43 -0400

    add comments

--- phone_number.py
+++ phone_number.py
@@ -1,6 +1,8 @@
+# Import the regex module with import re.
 import re
-# create regex pattern
+# Create a Regex object with the re.compile() function using a raw string
 phoneNumRegex = re.compile(r'\d{3}-\d{3}-\d{4}')
-# search using pattern
+# Pass the string you want to search into the Regex object’s search() method to return a Match object
 myNum = phoneNumRegex.search('My number is 949-459-4012.')
+# Call the Match object’s group() method to return a string of the actual matched text
 print('Phone number found: ' + myNum.group())
