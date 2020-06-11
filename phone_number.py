c6d5f94d9d8649e384bf455b155de9444fad5fcc
Anisa <anisahamohamed@gmail.com>
2020-06-10 20:31:00 -0400

    groupings

--- phone_number.py
+++ phone_number.py
@@ -6,3 +6,41 @@ phoneNumRegex = re.compile(r'\d{3}-\d{3}-\d{4}')
 myNum = phoneNumRegex.search('My number is 949-459-4012.')
 # Call the Match object’s group() method to return a string of the actual matched text
 print('Phone number found: ' + myNum.group())
+
+# Create g
+phoneNumRegex = re.compile(r'(\d{3})-(\d{3})-(\d{4})')
+# Pass the string you want to search into the Regex object’s search() method to return a Match object
+myNumGrouping = phoneNumRegex.search('My number is 949-459-4012.')
+print(myNumGrouping.group(1))
+print(myNumGrouping.group(2))
+print(myNumGrouping.group(3))
+print(myNumGrouping.groups())
+
+print(myNumGrouping.group(0))
+print(myNumGrouping.group())
+
+areaCode, mainNumberBeginning, mainNumberEnding = myNumGrouping.groups()
+
+print(areaCode)
+print(mainNumberBeginning)
+print(mainNumberEnding)
+#  escape characters
+# .  ^  $  *  +  ?  {  }  [  ]  \  |  (  )
+#  becomes
+# \.  \^  \$  \*  \+  \?  \{  \}  \[  \]  \\  \|  \(  \)
+phoneNumRegexParenthesis = re.compile(r'(\(\d\d\d\)) (\d\d\d-\d\d\d\d)')
+mo = phoneNumRegexParenthesis.search('My phone number is (415) 555-4242.')
+print(mo.group(1))
+print(mo.group(2))
+# Matching Multiple Groups with the Pipe -> |
+myNameRegex = re.compile(r'Anisa|George')
+mo1 = myNameRegex.search('Anisa and Apple and George')
+print(mo1.group())
+mo2 = myNameRegex.search("Apple and George and Anisa")
+
+print(mo2.group())
+
+batRegex = re.compile(r'Bat(man|mobile|copter|bat)')
+mo3 = batRegex.search('Batmobile lost a wheel')
+print(mo3.group())
+print(mo3.group(1))
